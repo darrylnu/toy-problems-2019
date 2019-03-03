e867cf426e38d5e12c9c945921d85161c3c0c0ca
Darryl Nunn <dnunn@Darryls-MacBook-Pro.local>
2019-03-03 17:53:23 -0600

    complete longestWord

--- src/CoderbyteEasy.java
+++ src/CoderbyteEasy.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class CoderbyteEasy {
 
     /*Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer. */
@@ -12,7 +15,16 @@ public class CoderbyteEasy {
     /*Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
      */
     static String longestWord(String sen) {
-        return sen;
+        String[] arrayOfStr = sen.split(" ");
+        Map<String, Integer> highCountMap = new HashMap<>();
+        highCountMap.put(arrayOfStr[0], arrayOfStr[0].length());
+        for(int i = 1; i < arrayOfStr.length; i++) {
+            if(arrayOfStr[i].length() > highCountMap.values().stream().findFirst().get()) {
+                highCountMap.remove(highCountMap.keySet().stream().findFirst().get());
+                highCountMap.put(arrayOfStr[i], arrayOfStr.length);
+            }
+        }
+        return highCountMap.keySet().stream().findFirst().get();
     }
 
     /*Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
--- test/CoderbyteEasyTests.java
+++ test/CoderbyteEasyTests.java
@@ -9,4 +9,10 @@ public class CoderbyteEasyTests {
         assert(output == 24);
     }
 
+    public void testLongestWord() {
+        String input = "I want to go home";
+        String output = CoderbyteEasy.longestWord(input);
+        assert(output.equals("want"));
+    }
+
 }
