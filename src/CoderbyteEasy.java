148fea86ccfe6075fde23489e6dd0b7e53d4a098
Darryl Nunn <dnunn@Darryls-MacBook-Pro.local>
2019-03-04 22:23:49 -0600

    checking to see if contribution will be tracked

--- src/CoderbyteEasy.java
+++ src/CoderbyteEasy.java
@@ -36,6 +36,7 @@ public class CoderbyteEasy {
         }
         return sb.toString();
     }
+    
 
     /*Have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
      */
